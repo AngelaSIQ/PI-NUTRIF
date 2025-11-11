4e5b64bc57df5e2e10565a195cd95ca772019aa9
ANGELA SIQUEIRA DE LIMA <20231041110029@ifrn.local>
2025-11-11 17:55:19 -0300

    atualizando commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import flask, render_template
+
+app = Flask(__name__)
+
+#pagina inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#login
+@app.route('/login')
+def login():
+    return render_template('login.html')   
+
+#cadastro
+@app.route('/cadastro/<usuario>')
+def cadastro(usuario):
+    return render_template('cadastro.html') 
+
+#cardápio   
+@app.route('/cardapio')
+def cardapio():
+    return render_template('cardapio.html')
+
+#feedback
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')   
+
+#receitas
+@app.route('/receitas/<prato>')
+def receitas():
+    return render_template('receitas.html') 
+
+#contatos
+@app.     
+
+
+
+
+if __name__ = "__main__"    
+    app.run
+
